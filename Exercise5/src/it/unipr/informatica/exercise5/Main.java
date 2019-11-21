f2dd6e083ca72549347d4f15289c6edd93ded69e
IceScreaM98 <AndreaMelioli@hotmail.it>
2019-11-21 13:48:29 +0100

    fix ai nomi non coerenti

--- Exercise5/src/it/unipr/informatica/exercise5/Main.java
+++ Exercise5/src/it/unipr/informatica/exercise5/Main.java
@@ -15,8 +15,8 @@ public class Main {
 			t.printStackTrace();
 			System.exit(-1);
 		}
-		String fn = "Il";
-		String n = "Gabibbo";
+		String fn = "Bravo";
+		String n = "Johnny";
 		try (
 			Connection connection = DriverManager.getConnection("jdbc:derby://127.0.0.1/Example");     //se vogliamo usare postgres.mysql,.... dobbiamo cambiare url
 			PreparedStatement statement = connection.prepareStatement("insert into STUDENT(FAMILYNAME, NAME) values (?, ?)")){
